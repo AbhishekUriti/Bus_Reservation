e091fe67b0a2739d488b86c70cfd42e69592b70f
AbhishekUriti <106744521+AbhishekUriti@users.noreply.github.com>
2023-06-20 14:58:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SeatBookingPage from './Components/SeatBookingPage';
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bus Seat Booking</h1>
+      <SeatBookingPage />
+    </div>
+  );
+};
+
+export default App;
